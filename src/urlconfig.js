80c9660118172d38d9ed66c5ac63edf60e7bab6b
Md Alif <md.alif10000@gmail.com>
2021-04-13 00:25:45 +0600

    changed url config

--- src/urlconfig.js
+++ src/urlconfig.js
@@ -1,5 +1,4 @@
-export const api = "https://hostomela-backend.herokuapp.com/api";
-export const generatePublicUrl=(fileName)=>{
-    return `https://hostomela-backend.herokuapp.com/public/${fileName}`;
-     
-}
+export const api = "https://hostomela-backend.herokuapp.com/api/";
+export const generatePublicUrl = (fileName) => {
+	return `https://hostomela-backend.herokuapp.com/public/${fileName}`;
+};
