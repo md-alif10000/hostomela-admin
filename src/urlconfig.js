5559593bcdce26c2b11ca67b87e42201490227f8
Md Alif <70016863+md-alif10000@users.noreply.github.com>
2021-04-24 11:11:14 +0600

    Update urlconfig.js

--- src/urlconfig.js
+++ src/urlconfig.js
@@ -1,8 +1,8 @@
-// export const api = "https://hostomela-backend.herokuapp.com/api/";
-export const api = "http://localhost:2000/api/";
-// export const generatePublicUrl = (fileName) => {
-// 	return `https://hostomela-backend.herokuapp.com/public/${fileName}`;
-// };
+export const api = "https://hostomela-backend.herokuapp.com/api/";
+// export const api = "http://localhost:2000/api/";
 export const generatePublicUrl = (fileName) => {
-	return `http://localhost:2000/public/${fileName}`;
+	return `https://hostomela-backend.herokuapp.com/public/${fileName}`;
 };
+// export const generatePublicUrl = (fileName) => {
+// 	return `http://localhost:2000/public/${fileName}`;
+// };
